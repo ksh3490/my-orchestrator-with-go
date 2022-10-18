e1c88b1b248bb864ded15116fe6ef46aacc8b840
ksh3490 <ksh349021@gmail.com>
2022-10-18 12:08:10 +0900

    Add test code for creating Task Event, Worker, Manager, Node object

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"my-orchestrator-with-go/manager"
+	"my-orchestrator-with-go/node"
 	"my-orchestrator-with-go/task"
+	"my-orchestrator-with-go/worker"
 
+	"fmt"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
 
@@ -17,5 +23,49 @@ func main() {
 		Disk:   1,
 	}
 
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+
 	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
+
+	w := worker.Worker{
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]task.Task),
+	}
+
+	fmt.Printf("worker: %v\n", w)
+
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDb:  make(map[string][]task.Task),
+		EventDb: make(map[string][]task.TaskEvent),
+		Workers: []string{w.Name},
+	}
+
+	fmt.Printf("manager: %v\n", m)
+
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("node: %v\n", n)
 }
