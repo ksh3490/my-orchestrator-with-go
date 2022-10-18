d525431f9fbf2ea73f86d069c4a1b971c5ed6828
ksh3490 <ksh349021@gmail.com>
2022-10-18 15:59:58 +0900

    Add Cores and Role in struct Node to match with the book

--- node/node.go
+++ node/node.go
@@ -3,9 +3,11 @@ package node
 type Node struct {
 	Name            string
 	Ip              string
+	Cores           int
 	Memory          int
 	MemoryAllocated int
 	Disk            int
 	DiskAllocated   int
+	Role            string
 	TaskCount       int
 }
