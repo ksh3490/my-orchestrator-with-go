4b4aab6ee2e583501366be7fd289899be1dac7fb
ksh3490 <ksh349021@gmail.com>
2022-10-18 16:01:46 +0900

    Add Name in struct Worker to match with the tutorials and import my-orchestrator-with-go/task to fix the reference error

--- worker/worker.go
+++ worker/worker.go
@@ -3,13 +3,16 @@ package worker
 import (
 	"fmt"
 
+	"my-orchestrator-with-go/task"
+
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
 
 type Worker struct {
+	Name      string
 	Queue     queue.Queue
-	Db        map[uuid.UUID]Task
+	Db        map[uuid.UUID]task.Task
 	TaskCount int
 }
 
