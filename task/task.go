9c577b9b8d09c669734d3b0ada5585e7f91e3ff9
ksh3490 <ksh349021@gmail.com>
2022-10-19 15:34:44 +0900

    Add struct Config used when running a container

--- task/task.go
+++ task/task.go
@@ -28,6 +28,19 @@ type TaskEvent struct {
 	Task      Task
 }
 
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
+
 type State int
 
 const (
