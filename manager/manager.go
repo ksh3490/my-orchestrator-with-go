a8a4686fc1b3b3f098f589ce48bc3956e524c4b4
ksh3490 <ksh349021@gmail.com>
2023-05-05 16:25:59 +0900

    Feature/Add func UpdateTasks

--- manager/manager.go
+++ manager/manager.go
@@ -38,7 +38,25 @@ func (m *Manager) SelectWorker() string {
 }
 
 func (m *Manager) UpdateTasks() {
-	fmt.Println("I will update tasks")
+	for _, worker := range m.Workers {
+		log.Printf("Checking worker %v for task updates", worker)
+		url := fmt.Sprintf("http://%s/tasks", worker)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Printf("Error connecting to %v: %v", worker, err)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Error sending request: %v", err)
+		}
+
+		d := json.NewDecoder(resp.Body)
+		var tasks []*task.Task
+		err = d.Decode(&tasks)
+		if err != nil {
+			log.Printf("Error unmarshalling tasks: %s", err.Error())
+		}
+	}
 }
 
 func (m *Manager) SendWork() {
